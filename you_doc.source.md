675cf56e8bd915a21ebd3a3bdffd7d489ef55224
Llewellyn Falco <isidore@setgame.com>
2019-05-24 08:40:52 -0700

    sample

---
+++ you_doc.source.md
@@ -0,0 +1,6 @@
+## Example
+
+Sometimes you need examples of source code in your documents
+
+snippet: my_example
+
---
+++ your_source.file.cs
@@ -0,0 +1,4 @@
+// snippet: my_example
+Integer[] numbers = {1, 2, 3};
+Feature.example(numbers, 4, 5);
+// snippet: end
